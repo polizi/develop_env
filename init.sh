a45ca768ab69c630461299a685bc1aae71f37549
ubuntu <ubuntu@localhost.localdomain>
2022-03-09 11:08:50 +0800

    install pkg

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 #neovim
+sudo apt-get install neovim
+sudo apt-get install python-neovim
+sudo apt-get install python3-neovim
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo npm install -g neovim
+sudo gem install neovim
+
 mkdir -p ~/.config
 cp -r nvim/nvim ~/.config
 
